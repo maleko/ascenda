589f78a11d99641e00b750037020eb7b2f188306
Marc Lee <marc@adviso.com.au>
2023-07-31 19:39:50 +1000

    initial Acccommodation model

---
+++ lib/services/translators/models/accommodation.rb
@@ -0,0 +1,39 @@
+require 'services/translators/models/generic_model' 
+require 'services/translators/models/facility'
+require 'services/translators/models/image'
+
+module Translators
+  module Models
+    class Accommodation < Translators::Models::GenericModel
+
+      attribute :id,              Shale::Type::String
+      attribute :destination_id,  Shale::Type::Integer
+      attribute :name,            Shale::Type::String
+      attribute :latitude,        Shale::Type::String
+      attribute :longitude,       Shale::Type::String
+      attribute :address,         Shale::Type::String
+      attribute :suburb,          Shale::Type::String
+      attribute :country,         Shale::Type::String
+      attribute :postcode,        Shale::Type::String
+      attribute :info,            Shale::Type::String 
+      attribute :facilities,      Translators::Models::Facility,  collection: true
+      attribute :images,          Translators::Models::Image,     collection: true
+      
+      def attributes
+        {
+          id:             id&.strip,
+          destination_id: destination_id,
+          name:           name&.strip,
+          latitude:       latitude,
+          longitude:      longitude,
+          address:        address&.strip,
+          suburb:         suburb&.strip,
+          country:        country&.strip,
+          postcode:       postcode&.strip,
+          info:           info&.strip
+        }.compact
+      end
+
+    end
+  end
+end
---
+++ spec/services/translators/models/accommodation_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+require 'services/translators/models/accommodation'
+
+RSpec.describe Translators::Models::Accommodation do
+  
+  describe "#attributes" do
+    let(:id)              { "ABSDE" } 
+    let(:destination_id)  { 123 }
+    let(:name)            { "Marriott" }
+    let(:latitude)        { 1.26 }
+    let(:longitude)       { 103.8 }
+    let(:address)         { "42 Escape to Nowhere"}
+    let(:suburb)          { "Shire" }
+    let(:country)         { "Singapore" }
+    let(:postcode)        { "343110" }
+    let(:info)            { "lorem ipsum dolor" }
+    
+    let(:json)            {
+      {
+        id:             id,
+        destination_id: destination_id,
+        name:           name, 
+        latitude:       latitude,
+        longitude:      longitude,
+        address:        address,
+        suburb:         suburb,
+        country:        country,
+        postcode:       postcode,
+        info:           info
+      }.to_json
+    }
+
+    subject { 
+      described_class.from_json(
+        json
+      )
+    }
+
+    context "attribute has trailing spaces" do
+      %w{id name address suburb country postcode info}.each do |attribute|
+        let(:example) { "42 Test Test   "}
+        let(attribute.to_sym) { example }
+
+        it "should trim trailing spaces for #{attribute}" do
+          expect(subject.attributes[attribute.to_sym]).to eql example.strip 
+        end
+      end
+    end
+
+    context "a context" do
+      %w{id name address suburb country postcode info}.each do |attribute|
+        let(:example) { nil }
+        let(attribute.to_sym) { example }
+
+        it "should not include empty attributes" do
+          expect(subject.attributes[attribute.to_sym]).to be_nil
+        end
+      end
+    end
+    
+  end
+  
+
+end
